4a58bf694f5fb8ec1806fa1dd7e658821999ea01
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-11-13 06:20:38 +0200

    fix print_number (handle zero)

--- print_number.c
+++ print_number.c
@@ -3,38 +3,40 @@
 /**
  * print_number - prints an integer.
  * @n: input integer.
- * Return: no return.
+ * Return: length of digit.
  */
 
 int print_number(int n)
 {
-	int digit_len = 0, m, d, count;
+	int digit_len = 0, m, count;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		m = -n;
 		digit_len++;
 	}
 	else
 		m = n;
 
-	d = m;
 	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
+	while (m / count > 9)
 		count *= 10;
-	}
 
-	for (; count >= 1; count /= 10)
-		_putchar(((m / count) % 10) + 48);
-
-	while (m != 0)
+	while (count > 0)
 	{
-		m /= 10;
+		_putchar((m / count) + '0');
 		digit_len++;
+		m %= count;
+		count /= 10;
 	}
+
 	return (digit_len);
 }
+
