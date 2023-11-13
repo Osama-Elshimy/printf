f56010dcea271907483d2f3c8c1f1d2107843514
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-11-13 12:38:13 +0200

    add convert_base function

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define PRINTF_HEADER_H
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,8 +11,9 @@ int _putchar(char c);
 size_t _strlen(const char *str);
 int print_string(const char *str);
 int print_invalid(char c);
-int print_number(int n);
+int print_number(int num);
 int print_binary(unsigned int n);
+char *convert_base(unsigned long number, int base, bool upper);
 
 int _printf(const char *format, ...);
 
